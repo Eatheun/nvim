2adfeee2fb98f12c483dfe1206680c00e6bee706
Ethan Loi <z5479926@ad.unsw.edu.au>
2024-10-29 11:56:02 +1100

    Fixed Grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,35 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	version = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
+  "nvim-telescope/telescope.nvim",
+  version = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      version = "^1.0.0",
+    },
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({})
+    telescope.load_extension("live_grep_args")
 
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	end,
+    local builtin = require("telescope.builtin")
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    keymap.set("n", "<Space><Space>", builtin.oldfiles, { desc = "Find old files" })
+    keymap.set(
+      "n",
+      "<leader>fg",
+      "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      { desc = "Live grep" }
+    )
+    keymap.set(
+      "n",
+      "<leader>gw",
+      require("telescope-live-grep-args.shortcuts").grep_word_under_cursor,
+      { desc = "grep word under cursor" }
+    )
+    keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+  end,
 }
