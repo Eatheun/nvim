5a3d05e06a6696d06dcb73d7aa5223880ae843e1
Ethan Loi <z5479926@ad.unsw.edu.au>
2024-11-20 01:30:37 +1100

    Added New Colour Palette Editing

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "rktjmp/lush.nvim", -- Or some other colorscheme
-    { dir = "~/.config/nvim/lua/coolio_colours", lazy = true },
-  },
-  {
-    "rktjmp/shipwright.nvim", -- For Lush
-    config = function()
-      vim.cmd([[colorscheme maccas-unhinged]])
-    end,
-  },
+  -- {
+  --   "rktjmp/lush.nvim", -- Or some other colorscheme
+  --   { dir = "~/.config/nvim/lua/coolio_colours", lazy = true },
+  -- },
+  -- {
+  --   "rktjmp/shipwright.nvim", -- For Lush
+  --   config = function()
+  --     vim.cmd([[colorscheme maccas-unhinged]])
+  --   end,
+  -- },
 }
---
+++ lua/plugins/palette.lua
@@ -0,0 +1,65 @@
+return {
+  "roobert/palette.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local red_mac = "#c31c2b"
+    local red_mac_light = "#d34c5b"
+    local red_mac_dark = "#950505"
+    local red_mac_black = "#420505"
+    local orange_mac = "#ff5800"
+    local orange_mac_dark = "#b93a04"
+    local yellow_mac = "#fdaf17"
+    local yellow_mac2 = "#ffce84"
+    local yellow_mac_light = "#fff0c0"
+    local yellow_mac_punchy = "#ffef00"
+    local green_mac = "#9dd304"
+    local green_mac_light = "#eaff61"
+    local green_mac_pale = "#7db304"
+    require("palette").setup({
+      palettes = {
+        main = "custom_main_palette",
+        accent = "custom_accent_palette",
+        state = "custom_state_palette",
+      },
+
+      custom_palettes = {
+        main = {
+          custom_main_palette = {
+            color0 = red_mac_black,
+            color1 = red_mac_dark,
+            color2 = orange_mac_dark,
+            color3 = yellow_mac_light,
+            color4 = orange_mac,
+            color5 = yellow_mac,
+            color6 = green_mac_pale,
+            color7 = yellow_mac,
+            color8 = yellow_mac2,
+          },
+        },
+        accent = {
+          custom_accent_palette = {
+            accent0 = red_mac_light,
+            accent1 = yellow_mac_punchy,
+            accent2 = yellow_mac,
+            accent3 = yellow_mac2,
+            accent4 = green_mac,
+            accent5 = red_mac,
+            accent6 = green_mac_light,
+          },
+        },
+        state = {
+          custom_state_palette = {
+            error = red_mac_light,
+            warning = orange_mac,
+            hint = yellow_mac2,
+            ok = green_mac,
+            info = green_mac_light,
+          },
+        },
+      },
+    })
+
+    vim.cmd([[colorscheme palette]])
+  end,
+}
