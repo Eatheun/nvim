94caf6a18f9e860432b85535db4f449bcdcadd78
Ethan Loi <z5479926@ad.unsw.edu.au>
2024-11-20 14:25:47 +1100

    Added Buffer Deletion and Buffer Underline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -15,8 +15,8 @@ return {
         left_mouse_command = "buffer %d", -- can be a string | function, | false see "Mouse actions"
         middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
         indicator = {
-          icon = "▎", -- this should be omitted if indicator style is not 'icon'
-          style = "icon", -- 'icon' | 'underline' | 'none',
+          -- icon = "▎", -- this should be omitted if indicator style is not 'icon'
+          style = "underline", -- 'icon' | 'underline' | 'none',
         },
         buffer_close_icon = "󰅖",
         modified_icon = "● ",
@@ -60,18 +60,18 @@ return {
         -- NOTE: this will be called a lot so don't do any heavy processing here
         custom_filter = function(buf_number, buf_numbers)
           -- filter out filetypes you don't want to see
-          -- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-          --   return true
-          -- end
+          if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+            return true
+          end
           -- filter out by buffer name
-          -- if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-          --   return true
-          -- end
+          if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+            return true
+          end
           -- filter out based on arbitrary rules
           -- e.g. filter out vim wiki buffer from tabline in your work repo
-          -- if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-          --   return true
-          -- end
+          if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+            return true
+          end
           -- filter out by it's index number in list (don't show first buffer)
           if buf_numbers[1] ~= buf_number then
             return true
@@ -129,6 +129,9 @@ return {
         },
       },
     })
+
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
   end,
   custom_areas = {
     right = function()
